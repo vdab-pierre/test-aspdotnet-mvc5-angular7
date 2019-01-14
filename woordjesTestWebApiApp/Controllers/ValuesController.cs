f555b765a53eec9a5728f53e40b562a4c4986781
vdab-pierre somerstraat <pierre.colin@vdab.be>
2019-01-14 11:44:46 +0100

    testje API

--- woordjesTestWebApiApp/Controllers/ValuesController.cs
+++ woordjesTestWebApiApp/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace woordjesTestWebApiApp.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "blah 1", "Blah 2" };
         }
 
         // GET api/values/5
