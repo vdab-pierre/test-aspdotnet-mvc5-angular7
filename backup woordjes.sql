0b84b4a955dbfe689dba16196387aa8f268c4818
pierre.colin <pierre.colin@FUJE920PIERRE>
2019-01-16 17:05:34 +0100

    backup db

---
+++ backup woordjes.sql
@@ -0,0 +1,3 @@
+﻿BACKUP DATABASE [woordjesTestService-20190114122455] TO  DISK = N'D:\Documents\bitbucket\woordjestestwebapi\woordjes.bak' WITH NOFORMAT, INIT,  NAME = N'Northwind_DB-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+
+GO
